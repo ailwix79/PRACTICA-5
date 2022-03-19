d3e90a36b5229563c2d4b5acdc12c9db1ffffc1b
ailwix79 <90765814+ailwix79@users.noreply.github.com>
2022-03-19 10:10:56 +0100

    Actuator Changes

--- src/main/java/com/main/practica5/Practica5Application.java
+++ src/main/java/com/main/practica5/Practica5Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.main.controller","com.main.services","com.main.utilities"})
+@ComponentScan({"com.main.controller","com.main.services","com.main.utilities","com.main.actuator"})
 public class Practica5Application {
 
 	public static void main(String[] args) {
